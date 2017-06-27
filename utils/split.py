e99ec76ce1f310f86cb400fbe966fa4c5977e0e1
LonSilent <101703044@nccu.edu.tw>
2017-06-27 14:35:52 +0800

    split

---
+++ utils/split.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    data_path = '../data/train-ec'
+    ratio = 0.8
+    line = 1000
+    train = line * ratio
+    test = line * (1 - ratio)
+
+    sentences = []
+    with open(data_path) as f:
+        for line in f:
+            sentences.append(line.strip())
+    with open('../data/train/train_{}.txt'.format(train), 'w') as f:
+        for s in sentences[:train]:
+            print(s, file=f)
+    with open('../data/test/test_gt_{}.txt'.format(test), 'w') as f:
+        for s in sentences[train:]:
+            print(s, file=f)
